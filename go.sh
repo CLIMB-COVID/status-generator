7999f64ad2f7f6f749ec474e44436d893db86a7a
Sam Nicholls <samstudio8@gmail.com>
2021-04-07 16:49:33 +0100

    Service status graph

--- go.sh
+++ go.sh
@@ -10,8 +10,13 @@ mkdir build/_includes
 
 DATE=`date -d $1 +'%Y-%m-%d'`
 
+# Load dat
 scp $PAG_TSV dat/pags.tsv
+scp $LAMPS_TSV dat/lamps.tsv
 
+
+# F1
+# To use source override commandArgs, e.g. commandArgs <- function(...) c("dat/pags.tsv", "2021-04-07"); source(...)
 Rscript f1-total_genomes/fig1.R dat/pags.tsv $DATE
 read TOTAL_PAGS TODAY_PAGS < <(head dat/fig1.dat)
 
@@ -20,6 +25,12 @@ cp dat/fig1c.png build/graphs/fig1c.png
 
 python f1-total_genomes/fig1.py $TOTAL_PAGS
 
+
+# F2
+Rscript f2-lamps/fig2.R dat/lamps.tsv $DATE
+cp dat/fig2.png build/graphs/fig2.png
+
+# Build
 ## Index
 sed "s,PLACEHOLDER_DATE,$DATE,g" template > build/index.md
 
