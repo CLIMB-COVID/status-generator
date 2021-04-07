9ef97f3f462ac050c5d61a5f53bbfbd04d1a9892
Sam Nicholls <samstudio8@gmail.com>
2021-04-07 01:17:11 +0100

    Add sidebar template

--- go.sh
+++ go.sh
@@ -6,16 +6,25 @@ mkdir dat
 rm -rf build
 mkdir build
 mkdir build/graphs
+mkdir build/_includes
+
+DATE=`date +'%Y-%m-%d'`
 
 scp $PAG_TSV dat/pags.tsv
 
-Rscript f1-total_genomes/fig1.R
-while IFS=$'\t' read -r TOTAL_PAGS; do
-    python f1-total_genomes/fig1.py $TOTAL_PAGS
-done < dat/fig1.dat
+Rscript f1-total_genomes/fig1.R dat/pags.tsv $DATE
+read TOTAL_PAGS TODAY_PAGS < <(head dat/fig1.dat)
 
-DATE=`date +'%Y-%m-%d'`
+python f1-total_genomes/fig1.py $TOTAL_PAGS
+
+## Index
 sed "s,PLACEHOLDER_DATE,$DATE,g" template > build/index.md
+
+## Sidebar data
+cat summary_template | \
+    sed "s,PLACEHOLDER_GENOMES_TODAY,$TODAY_PAGS,g" | \
+    sed "s,PLACEHOLDER_GENOMES_TOTAL,$TOTAL_PAGS,g" > \
+    build/_includes/summary.html
 
 mkdir build/assets
 cp -r assets/public/* build/assets
