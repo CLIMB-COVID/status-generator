7cfeeeef6e79442de1df12bf54a0a04f38bea4a4
Sam Nicholls <samstudio8@gmail.com>
2021-04-13 14:49:27 +0000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@ source .env
 
 read REPORT_DATE < <(head build/report_date)
 cp -r build/* $DEPLOY_DIR
+cd $DEPLOY_DIR
 git add .
-git commit -am "$DATE"
-git reset $(git commit-tree HEAD^{tree} -m "$DATE")
+git commit -am "$REPORT_DATE"
+git reset $(git commit-tree HEAD^{tree} -m "$REPORT_DATE")
 git push --force
+cd -
