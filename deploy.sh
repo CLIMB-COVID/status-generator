ac01eea0a072845c69e02e355b94adc5048bfbc1
Sam Nicholls <samstudio8@gmail.com>
2021-11-24 16:05:30 +0000

    Allow redeploy with empty commit

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ read REPORT_DATE < <(head build/report_date)
 cp -r build/* $DEPLOY_DIR
 cd $DEPLOY_DIR
 git add .
-git commit -am "$REPORT_DATE"
+git commit --allow-empty -am "$REPORT_DATE"
 git reset $(git commit-tree HEAD^{tree} -m "$REPORT_DATE")
 git push --force
 cd -
