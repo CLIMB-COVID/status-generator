95c52fef863ace0f6007de76dfd3d48f8310d4c1
Sam Nicholls <samstudio8@gmail.com>
2022-01-13 17:41:26 +0000

    remove .ocarina from status-generator

--- make_lamps.sh
+++ make_lamps.sh
@@ -1,4 +1,7 @@
-source ~/.ocarina
+source ~/.bootstrap.sh
+source $EAGLEOWL_CONF/paths.env
+source $EAGLEOWL_CONF/asklepian.env
+
 echo -e 'service\tdate' > dat/lamps.tsv
 
 ls $ASKLEPIAN_PUBDIR/ | grep '^20*' | sed 's,^,asklepian\t,' >> dat/lamps.tsv
