4a91e3c4f80547ee3948963404a6849e235b2c43
Sam Nicholls <samstudio8@gmail.com>
2022-02-08 17:20:51 +0000

    Use new elan dir for lamps

--- make_lamps.sh
+++ make_lamps.sh
@@ -5,5 +5,5 @@ source $EAGLEOWL_CONF/asklepian.env
 echo -e 'service\tdate' > dat/lamps.tsv
 
 ls $ASKLEPIAN_PUBDIR/ | grep '^20*' | sed 's,^,asklepian\t,' >> dat/lamps.tsv
-ls $COG_PUBLISHED_DIR/ | grep '^20*' | sed 's,^,elan\t,' >> dat/lamps.tsv
+ls $ARTIFACTS_ROOT/elan | grep '^20*' | sed 's,^,elan\t,' >> dat/lamps.tsv
 
